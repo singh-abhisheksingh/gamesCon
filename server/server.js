e0fc8cbf35beae6adc4d635310bfc84077832145
singh-abhisheksingh <abhisheksingh0058@gmail.com>
2019-03-04 00:35:33 +0530

    Handled CORS error

--- server/server.js
+++ server/server.js
@@ -7,6 +7,14 @@ const leaderboardRoutes = require('./routes/leaderboardRoutes');
 const port = process.env.PORT || 3000;
 var app = express();
 
+app.use(function(req,res,next){
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Expose-Headers', 'x-auth');
+  res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE");
+  res.setHeader('Access-Control-Allow-Headers','Origin, X-Requested-With,content-type, Accept , x-auth');
+  next();
+});
+
 app.use(bodyParser.urlencoded({extended : true}));
 app.use(bodyParser.json());
 
@@ -15,6 +23,12 @@ app.use('/leader', leaderboardRoutes)
 
 app.post('/', (req, res) => {
   console.log(req.body);
+});
+
+// var errorPg = path.join(__dirname, "../public/views/404.html");
+app.get("*", function(req,res){
+  // res.sendFile(errorPg);
+  res.send('404 Page Not found');
 });
 
 app.listen(port, () => {
