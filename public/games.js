d07503ab2d7a64ea5f47e1b1fb86a11f497c6965
siddhartha2107 <siddharthayadav202@gmail.com>
2019-03-05 21:17:44 +0530

    fixes+smartlock

--- public/games.js
+++ public/games.js
@@ -1,3 +1,3 @@
 localStorage.setItem("x-auth",'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6ImFkbWluIiwicGFzc3dvcmQiOiJuY3MyMDE4IiwiaWF0IjoxNTUxNjgzOTQyfQ.j-Ogo2W-WIZuJ9zsGIRzesyovnzRrIkGFv--Sku06xw');
-localStorage.setItem("postURL","http://192.168.1.9:8000/game/submit");
-localStorage.setItem("getURL","http://192.168.1.9:8000");
+localStorage.setItem("postURL","http://192.168.12.181:8000/game/submit");
+localStorage.setItem("getURL","http://192.168.12.181:8000");
